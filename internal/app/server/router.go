9176ff2fd88b46a7c53ce6412e68075abb8698e9
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-01-15 23:31:54 -0500

    Set up basic router

---
+++ internal/app/server/router.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func CreateRouter() {
+	wait := time.Duration(15)
+
+	r := mux.NewRouter()
+
+	srv := &http.Server{
+		Addr:         "0.0.0.0:80",
+		Handler:      r,
+		IdleTimeout:  time.Second * 60,
+		ReadTimeout:  time.Second * 15,
+		WriteTimeout: time.Second * 15,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			logrus.Error("Error on starting HTTP server:")
+			logrus.Error(err)
+		}
+	}()
+	logrus.Info("HTTP server now listening on port 80.")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Kill)
+
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	srv.Shutdown(ctx)
+	logrus.Info("HTTP server gracefully shut down.")
+}
