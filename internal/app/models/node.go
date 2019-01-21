ebd3d368678e0bc40d76795a34b787e162be0ee9
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-01-21 18:52:41 -0500

    Handle media playback on all nodes

--- internal/app/models/node.go
+++ internal/app/models/node.go
@@ -1,6 +1,10 @@
 package models
 
+import "time"
+
 type Node struct {
+	ticker *time.Ticker
+
 	ID uint
 
 	CurrentTime uint
@@ -12,5 +16,29 @@ func NewNode(id uint) *Node {
 		CurrentTime: 0,
 		ID: id,
 		Playlist: make([]Media, 0),
+		ticker: time.NewTicker(1 * time.Second),
 	}
+}
+
+func (n *Node) AddMedia(media Media) {
+	n.Playlist = append(n.Playlist, media)
+}
+
+func (n *Node) Run() {
+	for {
+		select {
+		case <- n.ticker.C:
+			if len(n.Playlist) > 0 {
+				n.CurrentTime += 1
+				if uint64(n.CurrentTime) > n.Playlist[0].Length {
+					n.Next()
+				}
+			}
+		}
+	}
+}
+
+func (n *Node) Next() {
+	n.Playlist = n.Playlist[1:]
+	n.CurrentTime = 0
 }
