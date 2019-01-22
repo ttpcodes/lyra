baaf7b0ab8a089ad88d8420fca70f6c850d18f16
Michael William Le Nguyen <michael@mail.ttp.codes>
2019-01-21 22:05:49 -0500

    Allow queueing of nonexistent media items

--- internal/app/server/ws/util/queue.go
+++ internal/app/server/ws/util/queue.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/mit6148/jma22-kvfrans-ttpcodes/internal/app/db"
+	"github.com/mit6148/jma22-kvfrans-ttpcodes/internal/app/sources/youtube"
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,8 +20,13 @@ func (c QueueCommand) Handle(client Client) {
 	}
 	media, err := db.GetMedia(c.ID)
 	if err != nil {
-		logrus.Warn("Error retrieving media by ID (is it correct):\n", err)
-		return
+		logrus.Warn("Media does not exist in database, creating.")
+		media, err = youtube.GetVideoInfo(c.ID)
+		if err != nil {
+			logrus.Warn("Error retrieving video info (is this a valid video):\n", err)
+			return
+		}
+		db.AddMedia(media)
 	}
 	client.User.Node.AddMedia(media)
 	logrus.Infof("Added media ID %s to node %d", c.ID, client.User.Node.ID)
