3a801e2bef50ff5883375a8aa83e724aef0a4f32
jma22 <chunming2000@gmail.com>
2019-01-29 01:02:25 -0500

    cookie monster

--- web/public/js/help.js
+++ web/public/js/help.js
@@ -22,3 +22,12 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+$(document).ready(function () {
+  //if cookie hasn't been set...
+  if (document.cookie.indexOf("ModalShown=true")<0) {
+      $("#myModal").css("display", "block");
+      };
+      document.cookie = "ModalShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+  }
+);
