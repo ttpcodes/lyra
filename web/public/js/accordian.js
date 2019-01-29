02e4bb0af7e5fcc860bf531bc214b5fb64770321
jma22 <chunming2000@gmail.com>
2019-01-28 21:20:37 -0500

    fixed accordian

--- web/public/js/accordian.js
+++ web/public/js/accordian.js
@@ -13,36 +13,32 @@ var accordians = document.getElementsByClassName("accordian");
 for (i = 0; i < 3; i++){
     accordians[i].onclick = function(){
         if( this.nextElementSibling.id == "map"){
-            
-            if(checkrepeats(tabsopen, 1)>=0)
-            {
-                tabsopen.splice(checkrepeats(tabsopen, 1), 1);
+            if (tabsopen.indexOf(1)>=0){
+                console.log(tabsopen)
+                tabsopen.splice(tabsopen.indexOf(1),1);
             }
             else{
                 tabsopen.push(1);
             }
         }
         if( this.nextElementSibling.id == "node"){
-            if(checkrepeats(tabsopen, 3)>=0)
-            {
-                tabsopen.splice(checkrepeats(tabsopen, 3), 1);
+            if (tabsopen.indexOf(3)>=0){
+                tabsopen.splice(tabsopen.indexOf(3),1);
             }
             else{
                 tabsopen.push(3);
             }
         }
         if( this.nextElementSibling.id == "playlist"){
-            if(checkrepeats(tabsopen, 2)>=0)
-            {
-                tabsopen.splice(checkrepeats(tabsopen, 2), 1);
+            if (tabsopen.indexOf(2)>=0){
+                tabsopen.splice(tabsopen.indexOf(2),1);
             }
             else{
                 tabsopen.push(2);
             }
+            
         }
-        if(tabsopen.length > 2){
-            tabsopen.splice(0, 1);
-        }
+        console.log(tabsopen);
         openthegates(tabsopen);    
     }
 }
@@ -51,50 +47,81 @@ function openthegates(array){
     one = 0;
     two = 0;
     three = 0;
-    for(var i =0; i< array.length; i++){
-        if (array[i] == 1){
-            map.style.maxHeight= map.scrollHeight +"px";
-            one = 1;
+    
+        for(var i =0; i< array.length; i++){
+            if (array.length == 1){
+                if (array[i] == 1){
+                    map.style.maxHeight= map.scrollHeight +"px";
+                    one = 1;
+                }
+                if (array[i] == 2){
+                    if (playlist.scrollheight < 630){
+                        playlist.style.maxHeight= playlist.scrollHeight +"px"; 
+                    }
+                    else {
+                        playlist.style.maxHeight = 630 + "px";
+                    }
+                    two = 1;
+                }
+                if (array[i] == 3){
+                    if (node.scrollheight < 630){
+                        node.style.maxHeight= node.scrollHeight +"px"; 
+                    }
+                    else {
+                        node.style.maxHeight = 630 + "px";
+                    }
+                    three = 1;
+                }
+            }
+            if (array.length == 2){
+                if (array[i] == 1){
+                    map.style.maxHeight= map.scrollHeight +"px";
+                    one = 1;
+                }
+                if (array[i] == 2){
+                    if (playlist.scrollheight < 315){
+                        playlist.style.maxHeight= playlist.scrollHeight +"px"; 
+                    }
+                    else {
+                        playlist.style.maxHeight = 315 + "px";
+                    }
+                    two = 1;
+                }
+                if (array[i] == 3){
+                    if (node.scrollheight < 315){
+                        node.style.maxHeight= node.scrollHeight +"px"; 
+                    }
+                    else {
+                        node.style.maxHeight = 315 + "px";
+                    }
+                    three = 1;
+                }
+            }
+            if (array.length == 3){
+                if (array[i] == 1){
+                    map.style.maxHeight= map.scrollHeight +"px";
+                    one = 1;
+                }
+                if (array[i] == 2){
+                    if (playlist.scrollheight < 175){
+                        playlist.style.maxHeight= playlist.scrollHeight +"px"; 
+                    }
+                    else {
+                        playlist.style.maxHeight = 175 + "px";
+                    }
+                    two = 1;
+                }
+                if (array[i] == 3){
+                    if (node.scrollheight < 175){
+                        node.style.maxHeight= node.scrollHeight +"px"; 
+                    }
+                    else {
+                        node.style.maxHeight = 175 + "px";
+                    }
+                    three = 1;
+                }
+            }
         }
-        if (array[i] == 2){
-            if (array.length<=1){
-                if (playlist.scrollheight< 600){
-                    playlist.style.maxHeight= playlist.scrollHeight +"px"; 
-                }
-                else {
-                    playlist.style.maxHeight = 600+"px";
-                }
-            }
-            else{
-                if (playlist.scrollheight< 300){
-                    playlist.style.maxHeight= playlist.scrollHeight +"px"; 
-                }
-                else {
-                    playlist.style.maxHeight = 300+"px";
-                }
-            }
-            two = 1; 
-        }
-        if (array[i] == 3){
-            if (array.length<=1){
-                if (node.scrollheight< 600){
-                    node.style.maxHeight= node.scrollHeight +"px"; 
-                }
-                else {
-                    node.style.maxHeight = 600+"px";
-                }
-            }
-            else{
-                if (node.scrollheight< 300){
-                    node.style.maxHeight= node.scrollHeight +"px"; 
-                }
-                else {
-                    node.style.maxHeight = 300+"px";
-                }  
-            }
-            three = 1;
-        }
-    }
 
     if( one==0){
         map.style.maxHeight =0;
@@ -107,12 +134,3 @@ function openthegates(array){
     }
 }
 
-function checkrepeats(array, num){
-    value = -1;
-    for(var i =0; i< array.length;i++){
-        if (array[i] == num){
-        value = i;
-        }
-    }
-    return value;
-}
